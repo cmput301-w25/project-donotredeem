698a08719b0c84bdd16709931a6a505380d96cf9
aditya-gupta05 <88316131+aditya-gupta05@users.noreply.github.com>
2025-02-19 17:24:25 -0700

    removing firebase imp

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -42,12 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-
-    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
-    implementation(libs.firebase.auth)
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.firestore)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
