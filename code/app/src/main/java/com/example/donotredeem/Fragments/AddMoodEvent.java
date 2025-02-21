8f68909e69a7ba8bcc6d2f990956fb7de1578604
Samridhi <samridhikhosla@gmail.com>
2025-02-20 22:20:46 -0700

    gallery permissions

--- code/app/src/main/java/com/example/donotredeem/Fragments/AddMoodEvent.java
+++ code/app/src/main/java/com/example/donotredeem/Fragments/AddMoodEvent.java
@@ -1,5 +1,6 @@
 package com.example.donotredeem.Fragments;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.Manifest;
@@ -21,6 +22,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -31,12 +33,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class AddMoodEvent extends Fragment {
 
-    private MoodType moods;
-    private Uri imageUri = null; //path to image from camera or gallery
+    //private MoodType moods;
+    //private Uri imageUri = null; //path to image from camera or gallery
     private ImageView image;
+
+   //when you request something android doesn't automatically know which request it belongs to.
+   //so you use a request code to match the response to the original request.
+
+    //permission requests
     private static final int CAMERA_REQUEST = 100;
-    private static final int STORAGE_REQUEST = 200;
+    private static final int GALLERY_REQUEST = 200;
     private ActivityResultLauncher<Intent> cameraLauncher;
+    private ActivityResultLauncher<Intent> galleryLauncher;
 
     public AddMoodEvent() {
 
@@ -46,20 +54,28 @@ public class AddMoodEvent extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //this below code is taken from https://developer.android.com/media/camera/camera-deprecated/photobasics
+
         cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == requireActivity().RESULT_OK && result.getData() != null) {
+
                 Bundle extras = result.getData().getExtras();
-                if (extras != null) {
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    if (imageBitmap != null) {
-                        image.setImageBitmap(imageBitmap);
-                    }
-                }
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                image.setImageBitmap(imageBitmap);
+
             }
         });
+
+        // change bitmap to uri after database is added
+
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == requireActivity().RESULT_OK && result.getData() != null) {
+                Uri imageUri = result.getData().getData();
+                image.setImageURI(imageUri);
+            }
+        });
+
     }
-
-
 
     @Nullable
     @Override
@@ -74,7 +90,9 @@ public class AddMoodEvent extends Fragment {
         image = view.findViewById(R.id.imageView);
         ImageButton media_upload = view.findViewById(R.id.upload_button);
 
-        media_upload.setOnClickListener(v -> checkCameraPermission());
+        media_upload.setOnClickListener(v -> {
+            showSourceDialog();
+        });
 
 
         //USING QUERY FIND JO BHI USER IS UPLOADING AND STORING IN DATABASE
@@ -82,10 +100,38 @@ public class AddMoodEvent extends Fragment {
         return view;
     }
 
+    private void showSourceDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_box_of_cam_gallery, null);
+
+        builder.setView(dialogView);
+
+        Button cameraButton = dialogView.findViewById(R.id.button_camera);
+        Button galleryButton = dialogView.findViewById(R.id.button_gallery);
+
+        cameraButton.setOnClickListener(v -> {
+            checkCameraPermission();
+        });
+
+        galleryButton.setOnClickListener(v -> {
+            checkGalleryPermission();
+        });
+        builder.create()
+                .show();
+    }
+
+
+
     private void checkCameraPermission() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
 
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            //https://developer.android.com/media/camera/camera-intents
+            //startActivityForResult is deprecated version
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //action of same or diff app
             cameraLauncher.launch(takePictureIntent);
 
         } else {
@@ -94,15 +140,15 @@ public class AddMoodEvent extends Fragment {
         }
     }
 
-//    private final ActivityResultLauncher<Intent> cameraLauncher =
-//            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-//                if (result.getResultCode() == getActivity().RESULT_OK && result.getData() != null) { //if the pic is clicked
-//                    Bundle extras = result.getData().getExtras();
-//                    Bitmap imageBitmap = (Bitmap) extras.get("data"); //captured image as bitmap
-//                    if (imageBitmap != null) {
-//                        image.setImageBitmap(imageBitmap);
-//                    }
-//                }
-//            });
+    private void checkGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED) {
+
+            Intent galleryOpenIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            galleryLauncher.launch(galleryOpenIntent);
+
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.READ_MEDIA_IMAGES}, GALLERY_REQUEST);
+        }
+    }
 
 }
