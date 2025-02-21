7fe6bf7aa81f6fb2c6ee3fe76f7335c803b55d7c
aditya-gupta05 <88316131+aditya-gupta05@users.noreply.github.com>
2025-02-20 20:40:07 -0700

    fixed users and profile page

--- code/app/src/main/java/com/example/donotredeem/Fragments/ProfilePage.java
+++ code/app/src/main/java/com/example/donotredeem/Fragments/ProfilePage.java
@@ -1,16 +1,21 @@
 package com.example.donotredeem.Fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.donotredeem.R;
 
-public class ProfilePage extends AppCompatActivity {
+public class ProfilePage extends Fragment {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile, container, false);
+        return view;
     }
 }
