2192da82d127ff511bb41dc72f38857cfea8c9eb
Ibrahim Sherwani <isherwan@ualberta.ca>
2025-03-08 12:57:12 -0700

    main_page javadoc

--- code/app/src/main/java/com/example/donotredeem/Fragments/MainPage.java
+++ code/app/src/main/java/com/example/donotredeem/Fragments/MainPage.java
@@ -23,11 +23,23 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+/**
+ * The {@code MainPage} class represents the main screen of the app after login.
+ * It displays the logged-in user's name and provides a sign-out option.
+ */
 public class MainPage extends Fragment {
 
     private FirebaseFirestore db;
     private String loggedInUsername;
 
+    /**
+     * Called to instantiate the fragment's view.
+     *
+     * @param inflater  The LayoutInflater used to inflate the layout.
+     * @param container The parent view that this fragment's UI should be attached to.
+     * @param savedInstanceState If non-null, the fragment is being re-constructed from a previous state.
+     * @return The root view of the fragment.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -64,6 +76,11 @@ public class MainPage extends Fragment {
         return view;
     }
 
+    /**
+     * Fetches the user details from Firestore and updates the UI with the logged-in username.
+     *
+     * @param textView The TextView where the username should be displayed.
+     */
     private void fetchUserDetails(TextView textView) {
         if (loggedInUsername == null) {
             Log.e("MainPageDebug", "loggedInUsername is null, redirecting to login");
@@ -87,6 +104,10 @@ public class MainPage extends Fragment {
                 });
     }
 
+    /**
+     * Logs out the user by signing out from Firebase and clearing login data.
+     * Redirects the user to the login screen.
+     */
     private void logout() {
         FirebaseAuth.getInstance().signOut();
 
@@ -100,6 +121,9 @@ public class MainPage extends Fragment {
         redirectToLogin();
     }
 
+    /**
+     * Redirects the user to the login screen and clears the activity stack.
+     */
     private void redirectToLogin() {
         Intent intent = new Intent(getActivity(), LogIn.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
